0b52fd4f811a1805c434efbf1a8d2300a6af46a0
Jriik89 <110338305+sadboy-dev@users.noreply.github.com>
2026-02-12 07:37:23 -0800

    Update instant.lua

--- instant.lua
+++ instant.lua
@@ -11,7 +11,7 @@ local NetFolder = ReplicatedStorage:WaitForChild("Packages")
 
 local ChargeFishingRod = NetFolder:WaitForChild("RF/ChargeFishingRod")
 local RequestFishingMinigame = NetFolder:WaitForChild("RF/RequestFishingMinigameStarted")
-local FishingCompleted = NetFolder:WaitForChild("RE/CatchFishCompleted")
+local FishingCompleted = NetFolder:WaitForChild("RF/CatchFishCompleted")
 local EquipToolFromHotbar = NetFolder:WaitForChild("RE/EquipToolFromHotbar")
 local CancelFishingInputs = NetFolder:WaitForChild("RF/CancelFishingInputs")
 local ReplicateTextEffect = NetFolder:FindFirstChild("RE/ReplicateTextEffect")
